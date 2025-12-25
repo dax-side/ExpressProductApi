c7971a0947de81ad6bafd7a475c4b93f5e1a3134
dax-side <damolaadegbite0@gmail.com>
2025-12-25 11:12:30 +0100

    feat: Remove partial update route from product routes

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -14,7 +14,6 @@ router.post('/', createProduct);
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
 router.put('/:id', updateProduct);
-router.patch('/:id', partialUpdateProduct);
 router.delete('/:id', deleteProduct);
 
 module.exports = router;
