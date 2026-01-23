f61c2a04d351c76ecc761cc5e89839f27614ea1e
dax-side <damolaadegbite0@gmail.com>
2026-01-23 16:29:00 +0100

    feat: Create product model with schema for name, price, description, and imageUrl

---
+++ models/product.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        default: null
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
