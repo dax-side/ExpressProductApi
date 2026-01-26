48de41ea2d0fc5c38680f9f31aaa64246306475e
dax-side <damolaadegbite0@gmail.com>
2026-01-26 09:31:59 +0100

    feat: Add route to fetch todo by ID in todo.routes.js

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { fetchAndSaveTodo, getAllTodos } = require('../controllers/todo.controller');
 
+router.get('/fetch/:id', fetchAndSaveTodo);
 router.get('/fetch', fetchAndSaveTodo);
 router.get('/', getAllTodos);
 
