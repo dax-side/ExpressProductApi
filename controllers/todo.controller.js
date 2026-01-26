437e3bb7459f7823da4d3dd0861d23fd1e02f3b8
dax-side <damolaadegbite0@gmail.com>
2026-01-26 09:31:50 +0100

    refactor: Simplify fetchAndSaveTodo function by removing comments and using dynamic todoId

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -3,23 +3,21 @@ const Todo = require('../models/todo.model');
 
 const fetchAndSaveTodo = async (req, res) => {
     try {
-        // Call external API
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+        const todoId = req.params.id || 1;
+        
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${todoId}`);
         
         const todoData = response.data;
 
-        // Check if todo already exists
         let todo = await Todo.findOne({ id: todoData.id });
 
         if (todo) {
-            // If exists, return the existing one
             return res.status(200).json({ 
                 message: 'Todo already exists in database', 
                 todo 
             });
         }
 
-        // Save to database
         todo = new Todo({
             userId: todoData.userId,
             id: todoData.id,
